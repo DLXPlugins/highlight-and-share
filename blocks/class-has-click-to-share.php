ecf233d905c86af6ceed4eafb31992627e6066cf
Ronald Huereca <rhuereca@updraftplus.com>
2019-01-15 18:37:53 -0600

    Adding temporary Gutenberg block.

--- blocks/class-has-click-to-share.php
+++ blocks/class-has-click-to-share.php
@@ -3,7 +3,7 @@
 /**
  * Registers the `core/latest-posts` block on server.
  */
-function ptam_register_custom_posts_block() {
+function has_register_block_attributes() {
 
 	// Check if the register function exists
 	if ( ! function_exists( 'register_block_type' ) ) {
@@ -132,5 +132,36 @@ function ptam_register_custom_posts_block() {
 function has_click_to_share( $attributes ) {
 	return 'hello';
 }
+
+/**
+ * Enqueue assets for frontend and backend
+ *
+ * @since 1.0.0
+ */
+function has_blocks_block_assets() {
+
+	// Load the compiled styles
+	wp_enqueue_style(
+		'has-style-css',
+		Highlight_And_Share::get_instance()->get_plugin_url( 'assets/dist/css/admin.css'),
+		HIGHLIGHT_AND_SHARE_VERSION, 'all' );
+}
+add_action( 'enqueue_block_assets', 'has_blocks_block_assets' );
+
+/**
+ * Enqueue assets for backend editor
+ *
+ * @since 1.0.0
+ */
+function has_blocks_editor_assets() {
+
+	// Load the compiled blocks into the editor
+	wp_enqueue_script(
+		'has-click-to-share-gutenberg',
+		Highlight_And_Share::get_instance()->get_plugin_url( 'assets/dist/js/gutenberg.js'),
+		array( 'wp-blocks', 'wp-element' ), HIGHLIGHT_AND_SHARE_VERSION, true
+	);
+}
+add_action( 'enqueue_block_editor_assets', 'has_blocks_editor_assets' );
 
 add_action( 'init', 'has_register_block_attributes' );
