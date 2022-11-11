5c653994c09516e2d3872183f45b318acd81d057
Ronald Huereca <ronald@mediaron.com>
2022-11-11 11:14:32 -0600

    Adding local google fonts.

---
+++ src/fonts/fonts.js
@@ -0,0 +1,58 @@
+const fontFamilies = {
+	Roboto: {
+		name: 'Roboto',
+		slug: 'roboto',
+		family: 'Roboto',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	'Josefin Sans': {
+		name: 'Josefin Sans',
+		slug: 'josefin-sans',
+		family: 'Josefin Sans',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	Karla: {
+		name: 'Karla',
+		slug: 'karla',
+		family: 'Karla',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	Lato: {
+		name: 'Lato',
+		slug: 'lato',
+		family: 'Lato',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	Montserrat: {
+		name: 'Montserrat',
+		slug: 'montserrat',
+		family: 'Montserrat',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	'Open Sans': {
+		name: 'Open Sans',
+		slug: 'open-sans',
+		family: 'Open Sans',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	'Playfair Display': {
+		name: 'Playfair Display',
+		slug: 'playfair-display',
+		family: 'Playfair Display',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	Raleway: {
+		name: 'Raleway',
+		slug: 'raleway',
+		family: 'Raleway',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+	'Source Sans Pro': {
+		name: 'Source Sans Pro',
+		slug: 'source-sans-pro',
+		family: 'Source Sans Pro',
+		fallback: 'Helvetica, Arial, sans-serif',
+	},
+};
+
+export default fontFamilies;
