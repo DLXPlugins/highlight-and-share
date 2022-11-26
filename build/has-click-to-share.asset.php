e33717b80b1bb03d58a0c53c0a305536e9a60b54
Ronald Huereca <ronald@mediaron.com>
2022-11-25 19:20:52 -0600

    Doing prod build.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3f2d890ae4d14c63f7fe');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2446d71819710f78e8a4');
