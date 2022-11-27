ab4d374c8f8386187fd4a6ce180f6f7c18ba74ae
Ronald Huereca <ronald@mediaron.com>
2022-11-27 16:29:08 -0600

    Modify positioning in the admin.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7c669dc9bee0368b93b8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8eef88f02ffe38d91337');
