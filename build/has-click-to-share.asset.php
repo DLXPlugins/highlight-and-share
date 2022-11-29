f7870f093e12be675cc76d5e09a54fb654f6be28
Ronald Huereca <ronald@mediaron.com>
2022-11-29 13:46:23 -0600

    Changing block description.

--- build/has-click-to-share.asset.php
+++ build/has-click-to-share.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0e663d276edffc99e9a9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8e89a41e74c83c22c55');
